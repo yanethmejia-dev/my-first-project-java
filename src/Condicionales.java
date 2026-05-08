738857f2a966a3fc5ababd306eb2fb6d116f5799
yanethmejia-dev <yaneth.mejia@ucaldas.edu.co>
2026-05-08 09:24:13 -0500

    feat: nuevo archivo Condicionales

---
+++ src/Condicionales.java
@@ -0,0 +1,12 @@
+public class Condicionales {
+    static int numero1 = 10;
+    static int numero2 = 20;
+
+    public static void main(String[] args) {
+        if (numero1 > numero2) {
+            System.out.println(numero1 + " es mayor que " + numero2);
+        }else{
+            System.out.println(numero2 + " es mayor que " + numero1);
+        }
+    }
+}
